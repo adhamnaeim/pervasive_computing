0159e2bcd443ba67fbafe1e33baf00cc460a3d41
Jususs <j.warm@gmx.at>
2026-04-28 11:56:33 +0200

    added timestamp

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -4,10 +4,11 @@ evtSource.onmessage = function(event) {
     const data = JSON.parse(event.data);
     console.log("New measurement received:", data);
     
+    document.getElementById("timestamp").innerText = "Last updated: " + data.ts ?? "N/A";
     document.getElementById("temp-sensor").innerText = data.temp_c + " °C" ?? "N/A";
-    document.getElementById("humidity-sensor").innerText = data.humidity + " %"?? "N/A";
-    document.getElementById("dust-sensor").innerText = data.dust_pcs + " ppm"?? "N/A";
-    document.getElementById("co2-sensor").innerText = data.co2_ppm + " ppm"?? "N/A";
+    document.getElementById("humidity-sensor").innerText = data.humidity + " %" ?? "N/A";
+    document.getElementById("dust-sensor").innerText = data.dust_pcs + " ppm" ?? "N/A";
+    document.getElementById("co2-sensor").innerText = data.co2_ppm + " ppm" ?? "N/A";
 };
 
 evtSource.onerror = function(err) {
