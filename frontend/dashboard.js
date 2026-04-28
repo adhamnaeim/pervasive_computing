3009c87a133a3d128e922c55fa6ac6af0b4a5c71
Jususs <j.warm@gmx.at>
2026-04-28 11:38:09 +0200

    Added units

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -4,10 +4,10 @@ evtSource.onmessage = function(event) {
     const data = JSON.parse(event.data);
     console.log("New measurement received:", data);
     
-    document.getElementById("temp-sensor").innerText = data.temp_c ?? "N/A";
-    document.getElementById("humidity-sensor").innerText = data.humidity ?? "N/A";
-    document.getElementById("dust-sensor").innerText = data.dust_pcs ?? "N/A";
-    document.getElementById("co2-sensor").innerText = data.co2_ppm ?? "N/A";
+    document.getElementById("temp-sensor").innerText = data.temp_c + " °C" ?? "N/A";
+    document.getElementById("humidity-sensor").innerText = data.humidity + " %"?? "N/A";
+    document.getElementById("dust-sensor").innerText = data.dust_pcs + " ppm"?? "N/A";
+    document.getElementById("co2-sensor").innerText = data.co2_ppm + " ppm"?? "N/A";
 };
 
 evtSource.onerror = function(err) {
