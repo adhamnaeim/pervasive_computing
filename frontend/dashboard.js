0db2b0a02bb3e3693e2993f3b80d6f05208c5cff
Jususs <j.warm@gmx.at>
2026-04-30 13:05:36 +0200

    refactored data

--- frontend/dashboard.js
+++ frontend/dashboard.js
@@ -4,11 +4,11 @@ evtSource.onmessage = function(event) {
     const data = JSON.parse(event.data);
     console.log("New measurement received:", data);
     
-    document.getElementById("timestamp").innerText = "Last updated: " + data.ts ?? "N/A";
-    document.getElementById("temp-sensor").innerText = data.temp_c + " °C" ?? "N/A";
-    document.getElementById("humidity-sensor").innerText = data.humidity + " %" ?? "N/A";
-    document.getElementById("dust-sensor").innerText = data.dust_pcs + " ppm" ?? "N/A";
-    document.getElementById("co2-sensor").innerText = data.co2_ppm + " ppm" ?? "N/A";
+    document.getElementById("timestamp").innerText = data.ts != null ? `Last updated: ${data.ts}` : "N/A";
+    document.getElementById("temp-sensor").innerText = data.temp_c != null ? `${data.temp_c} °C` : "N/A";
+    document.getElementById("humidity-sensor").innerText = data.humidity != null ? `${data.humidity} %` : "N/A";
+    document.getElementById("dust-sensor").innerText = data.dust_pcs != null ? `${data.dust_pcs} ppm` : "N/A";
+    document.getElementById("co2-sensor").innerText = data.co2_ppm != null ? `${data.co2_ppm} ppm` : "N/A";
 };
 
 evtSource.onerror = function(err) {
